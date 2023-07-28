4b07d3fa12788a67c9fa6bdb21257c56d921f4e5
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-28 17:10:48 -0300

    feat: Ajusted page whit header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Header } from "@/components/Header";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <p>Portfolio Vagner Nerves</p>
+    <main className="">
+      <Header />
+      <div id="home" className="pt-24 min-h-screen">
+        <p className="text-zinc-300">Portfolio Vagner Nerves</p>
+      </div>
+      <div id="about" className="pt-24 min-h-screen">
+        <p className="text-zinc-300">About</p>
+      </div>
+      <div id="project" className="pt-24 min-h-screen">
+        <p className="text-zinc-300">Project</p>
+      </div>
+      <div id="contact" className="pt-24 min-h-screen">
+        <p className="text-zinc-300">Contact</p>
+      </div>
     </main>
   );
 }
