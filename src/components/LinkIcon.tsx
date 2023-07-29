2d3c7ad851bdb7e5c3d41e66fe29282d5c0ed96a
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-29 11:57:24 -0300

    feat: Created component LinkIcon.

---
+++ src/components/LinkIcon.tsx
@@ -0,0 +1,27 @@
+import { LinkedinLogo, GithubLogo } from "@/assets/icons/PhosphorIcons";
+import Link from "next/link";
+
+interface LinkIconProps {
+  href: string;
+  typeIcons: "linkedin" | "github";
+}
+export function LinkIcon({ href, typeIcons }: LinkIconProps) {
+  return (
+    <Link href={href} target="_blank">
+      {typeIcons === "linkedin" && (
+        <LinkedinLogo
+          weight="bold"
+          size={26}
+          className="fill-zinc-800 hover:fill-zinc-300"
+        />
+      )}
+      {typeIcons === "github" && (
+        <GithubLogo
+          weight="bold"
+          size={26}
+          className="fill-zinc-800 hover:fill-zinc-300"
+        />
+      )}
+    </Link>
+  );
+}
