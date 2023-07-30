4c8488dfcbe9e95917668f9fb5c3a444bb905b66
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-30 08:42:29 -0300

    feat: Ajusted Carousel in Project.

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,28 +1,34 @@
 import { ButtonLink } from "./ButtonLink";
+import { Carousel } from "./Carousel";
 
 interface ProjectProps {
   title: string;
-  data: string;
+  date: string;
   description: string[];
   technologies: string[];
   url: string;
+  images: {
+    alt: string;
+    src: string;
+  }[];
 }
 export function Project({
   title,
-  data,
+  date,
   description,
   technologies,
   url,
+  images,
 }: ProjectProps) {
   return (
-    <div className="group/project py-9 flex flex-1 gap-2">
+    <div className="group/project py-12 flex flex-1 gap-2">
       <div className="flex flex-1 flex-col relative">
         <div
           className="w-6 h-6 rounded-full bg-zinc-950 border-2 border-blue-800 absolute top-1 -left-[52px] 
     group-hover/project:bg-dashed group-hover/project:bg-cover "
         />
         <h3 className="font-bold text-xl text-zinc-50 leading-8">{title}</h3>
-        <p className="font-normal text-xs text-zinc-400 leading-8">{data}</p>
+        <p className="font-normal text-xs text-zinc-400 leading-8">{date}</p>
         <div className="font-normal text-base text-zinc-400 leading-8 space-y-5">
           {description.map((value, index) => (
             <p key={index}>{value}</p>
@@ -36,8 +42,8 @@ export function Project({
           <ButtonLink typeIcon="github" title="Acesse o Projeto" url={url} />
         </div>
       </div>
-      <div className="min-w-[400px] flex items-center justify-center bg-zinc-500">
-        <p className="text-zinc-200">photo</p>
+      <div className="min-w-[400px] flex items-center justify-center ">
+        <Carousel data={images} />
       </div>
     </div>
   );
--- src/components/SectionProjects.tsx
+++ src/components/SectionProjects.tsx
@@ -1,4 +1,3 @@
-import { ButtonLink } from "./ButtonLink";
 import { Project } from "./Project";
 import { SubTitle } from "./SubTitle";
 
@@ -14,7 +13,7 @@ export function SectionProjects() {
           <div className="border-l border-blue-800 pl-10 flex flex-1 flex-col">
             <Project
               title="Ignite Gym"
-              data="(Junho 2023 - Agosto 2023)"
+              date="(Junho 2023 - Agosto 2023)"
               description={[
                 "O projeto Ignite Gim foi desenvolvido em React Native e é um aplicativo voltado para fitness que oferece recursos de registro de usuários, rastreamento de  exercícios e histórico de execução.",
                 "Nele foi criado as seguintes telas: Tela de Login, Tela de Registro, Tela Principal, Histórico de Treino, Tela de  Edição Perfil.",
@@ -34,6 +33,49 @@ export function SectionProjects() {
                 "AsyncStorage",
               ]}
               url="https://github.com/VagnerNerves/ignitegym-rn"
+              images={[
+                {
+                  alt: "Image 1",
+                  src: "/images/project-ignite-gym/011212-home-project-ignit-gym.png",
+                },
+                {
+                  alt: "Image 2",
+                  src: "/images/project-ignite-gym/2-home-project-ignit-gym.png",
+                },
+              ]}
+            />
+            <Project
+              title="Ignite Gym"
+              date="(Junho 2023 - Agosto 2023)"
+              description={[
+                "O projeto Ignite Gim foi desenvolvido em React Native e é um aplicativo voltado para fitness que oferece recursos de registro de usuários, rastreamento de  exercícios e histórico de execução.",
+                "Nele foi criado as seguintes telas: Tela de Login, Tela de Registro, Tela Principal, Histórico de Treino, Tela de  Edição Perfil.",
+                "Essas telas proporcionam uma experiência abrangente para os usuários do aplicativo Ignite Gim, permitindo que eles façam login, se registrem, realizem exercícios, acompanhem seu progresso e gerenciem suas informações pessoais.",
+              ]}
+              technologies={[
+                "React Native",
+                "Expo",
+                "TypeScript",
+                "NativeBase",
+                "React Navigation",
+                "Axios",
+                "Expo ImagePicker",
+                "Expo FileSystem",
+                "React Hook Form",
+                "Yup",
+                "AsyncStorage",
+              ]}
+              url="https://github.com/VagnerNerves/ignitegym-rn"
+              images={[
+                {
+                  alt: "Image 1",
+                  src: "/images/project-ignite-gym/011212-home-project-ignit-gym.png",
+                },
+                {
+                  alt: "Image 2",
+                  src: "/images/project-ignite-gym/2-home-project-ignit-gym.png",
+                },
+              ]}
             />
           </div>
         </div>
