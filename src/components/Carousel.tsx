3214cdb9b87a2839b0776e78280a7917700598f3
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-30 08:34:49 -0300

    feat: Created Carousel.

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+import { EffectCards } from "swiper/modules";
+import Image from "next/image";
+
+interface CarouselProps {
+  data: {
+    alt: string;
+    src: string;
+  }[];
+}
+export function Carousel({ data }: CarouselProps) {
+  return (
+    <Swiper
+      effect={"cards"}
+      grabCursor={true}
+      modules={[EffectCards]}
+      className="w-[260px] h-[560px]"
+    >
+      {data.map((value, index) => {
+        return (
+          <SwiperSlide key={index} className="rounded-2xl overflow-hidden">
+            <Image
+              alt={value.alt}
+              fill={true}
+              style={{ objectFit: "cover" }}
+              src={value.src}
+            />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
