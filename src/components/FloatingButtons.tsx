f5c330f3615c64183517d43c2c507d3f48bf838a
Vagner Nerves <vagnernervessantos@gmail.com>
2023-08-14 22:43:46 -0300

    feat: Ajusted Floating button.

--- src/components/FloatingButtons.tsx
+++ src/components/FloatingButtons.tsx
@@ -7,55 +7,26 @@ import flagBrazilPng from '@/assets/iconFlagBrazil.png'
 import flagUsPng from '@/assets/iconFlagUS.png'
 
 export function FloatingButtons() {
-  const [isOptionsVisible, setIsOptionsVisible] = useState<boolean>(false)
-
   const { t, language, saveLanguage } = useTranslation()
   const translation = t()
 
   function handleAlterLanguage() {
     saveLanguage(language === 'pt-BR' ? 'en' : 'pt-BR')
-    setIsOptionsVisible(false)
   }
 
   return (
-    <>
-      <button
-        title={translation.floatingButtons.buttonOptions}
-        onClick={() => setIsOptionsVisible(isCurrent => !isCurrent)}
-        data-active={isOptionsVisible}
-        className="z-20 fixed bottom-24 sm:bottom-9 right-3 sm:right-9 w-10 h-10 rounded-full bg-zinc-950 border border-zinc-800 text-zinc-500
-        font-semibold text-base flex justify-center items-center text-center
-        data-[active=true]:text-zinc-200 data-[active=true]:border-blue-900 data-[active=true]:bg-blue-900
-         hover:text-zinc-200 hover:border-blue-900 hover:bg-blue-900"
-      >
-        {isOptionsVisible ? <span>-</span> : <span>+</span>}
-      </button>
-      {isOptionsVisible && (
-        <>
-          <button
-            title={translation.floatingButtons.buttonTranslate}
-            onClick={handleAlterLanguage}
-            className="z-20 fixed bottom-32 sm:bottom-20 right-11 sm:right-14 w-10 h-10 rounded-full bg-zinc-950 border border-zinc-800 text-zinc-500
-        font-semibold text-base flex justify-center items-center text-center hover:text-zinc-400 hover:border-zinc-700"
-          >
-            <Image
-              src={language === 'pt-BR' ? flagUsPng : flagBrazilPng}
-              alt={translation.floatingButtons.altImageTranslate}
-              width={34}
-              className="filter grayscale hover:grayscale-0"
-              priority={true}
-            />
-          </button>
-
-          {/* <button
-            title="Traduzir para Português Brasil."
-            className="z-20 fixed bottom-10 right-20 w-10 h-10 rounded-full bg-zinc-950 border border-zinc-800 text-zinc-500
-        font-semibold text-base flex justify-center items-center text-center hover:text-zinc-400 hover:border-zinc-700"
-          >
-            +
-          </button> */}
-        </>
-      )}
-    </>
+    <button
+      title={translation.floatingButtons.buttonTranslate}
+      onClick={handleAlterLanguage}
+      className="z-20 fixed bottom-[100px] sm:bottom-9 right-3 sm:right-9 w-8 h-8 rounded-full bg-zinc-950 border border-zinc-800 text-zinc-500 font-semibold text-base flex justify-center items-center text-center hover:text-zinc-400 hover:border-zinc-700"
+    >
+      <Image
+        src={language === 'pt-BR' ? flagUsPng : flagBrazilPng}
+        alt={translation.floatingButtons.altImageTranslate}
+        width={28}
+        className="filter grayscale hover:grayscale-0"
+        priority={true}
+      />
+    </button>
   )
 }
