4d2678704bff210e2b3f9f1de84833e183d9d65e
Vagner Nerves <vagnernervessantos@gmail.com>
2024-10-30 17:04:48 -0300

    feat: Add icon Globe.

--- src/assets/icons/PhosphorIcons.tsx
+++ src/assets/icons/PhosphorIcons.tsx
@@ -6,7 +6,8 @@ import {
   GithubLogo,
   At,
   House,
-  User
+  User,
+  Globe
 } from '@phosphor-icons/react'
 
-export { CodeBlock, LinkedinLogo, GithubLogo, At, House, User }
+export { CodeBlock, LinkedinLogo, GithubLogo, At, House, User, Globe }
