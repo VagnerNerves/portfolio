bee501eb0237774594b983326f9bbdfad0b4b538
Vagner Nerves <vagnernervessantos@gmail.com>
2024-12-27 12:25:53 -0300

    feat: Ajsuted TypeWriter for return span.

--- src/components/TypewriterText.tsx
+++ src/components/TypewriterText.tsx
@@ -5,12 +5,7 @@ import { Typewriter } from 'react-simple-typewriter'
 
 interface TypewriterTextProps {
   text: string[]
-  divProps?: HtmlHTMLAttributes<HTMLDivElement>
 }
-export function TypewriterText({ text, divProps }: TypewriterTextProps) {
-  return (
-    <div {...divProps}>
-      <Typewriter words={text} cursor loop={false} delaySpeed={10000} />
-    </div>
-  )
+export function TypewriterText({ text }: TypewriterTextProps) {
+  return <Typewriter words={text} cursor loop={false} delaySpeed={10000} />
 }
